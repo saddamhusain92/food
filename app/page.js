31be1c9f625f4c9b9505bc87993f3f64f40c3b45
saddam khan <saddamkhan.khan705@gmail.com>
2025-05-16 15:22:11 +0530

    initial space fixed

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-24 gap-[6rem]">
+    <div className="flex flex-col items-center justify-center min-h-screen p-4 gap-[6rem]">
       <img src="https://try.bricksbuilder.io/tff409ff/wp-content/uploads/sites/35758/2025/05/7d307e3f6aa845d7b8e3190ed2954384.jpg" alt="Next.js logo" width="80%" height="100%"  />
       <img src="https://try.bricksbuilder.io/tff409ff/wp-content/uploads/sites/35758/2025/05/Restaurant-Menu-Design.jpg" alt="Next.js logo" width="80%" height="100%"  />
       <img src="https://try.bricksbuilder.io/tff409ff/wp-content/uploads/sites/35758/2025/05/b233ee6acf40abaa13aa129b7ee48a1c.jpg" alt="Next.js logo" width="80%" height="100%"  />
